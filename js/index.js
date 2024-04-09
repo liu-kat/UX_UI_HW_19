8bd5ff88311b0d2304ea41158cf1129f0d62b4e6
liu-kat <katherineliu25@gmail.com>
2024-04-09 01:56:03 -0700

    make buttons change color on mouseover

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$("button").on("mouseenter", function () {
+  $(this).css("background-color", "#8c724d");
+});
+$("button").on("mouseleave", function () {
+  $(this).css("background-color", "#373738");
+});
